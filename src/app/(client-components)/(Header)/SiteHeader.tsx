c76939c43b72ad47b8b3f07dbf623d473beb100d
System Administrator <root@PARTHKUMARs-MacBook-Air.local>
2024-08-02 06:31:05 +0530

    Inquiry Dialog

--- src/app/(client-components)/(Header)/SiteHeader.tsx
+++ src/app/(client-components)/(Header)/SiteHeader.tsx
@@ -5,13 +5,25 @@ import {
 	ShoppingBagIcon as ShoppingCartIcon,
 	Cog8ToothIcon as CogIcon,
 } from '@heroicons/react/24/outline'
-import { Popover, Transition } from '@headlessui/react'
+import {
+	Dialog,
+	DialogTitle,
+	Popover,
+	Transition,
+	TransitionChild,
+} from '@headlessui/react'
 import { PathName } from '@/routers/types'
 import Link from 'next/link'
 import Header from './Header'
 import Header3 from './Header3'
 import { usePathname } from 'next/navigation'
 import { useThemeMode } from '@/utils/useThemeMode'
+import ButtonClose from '@/shared/ButtonClose'
+import ButtonThird from '@/shared/ButtonThird'
+import ButtonPrimary from '@/shared/ButtonPrimary'
+import Label from '@/components/Label'
+import Input from '@/shared/Input'
+import Textarea from '@/shared/Textarea'
 
 export type SiteHeaders = 'Header 1' | 'Header 2' | 'Header 3'
 
@@ -34,6 +46,10 @@ const PAGES_HIDE_HEADER_BORDER: PathName[] = [
 ]
 
 const SiteHeader = () => {
+	const [isOpenMoreFilter, setisOpenMoreFilter] = useState(false)
+	const closeModalMoreFilter = () => setisOpenMoreFilter(false)
+	const openModalMoreFilter = () => setisOpenMoreFilter(true)
+
 	const anchorRef = useRef<HTMLDivElement>(null)
 
 	let [headers] = useState<SiteHeaders[]>(['Header 1', 'Header 2', 'Header 3'])
@@ -181,24 +197,141 @@ const SiteHeader = () => {
 		)
 	}
 
-		// FOR DEMO PAGE
+	// FOR DEMO PAGE
 	const renderInquiryForm = () => {
 		return (
 			<div className="ControlSelections relative z-40 hidden lg:block">
-		        <div className='fixed right-3 top-1/2 z-40 flex items-center'>				<Popover className="relative">
+				<div className="fixed right-3 top-1/2 z-40 flex items-center">
+					<Popover className="relative">
 						{({ open }) => (
 							<>
 								<Popover.Button
-									className={`z-10 rounded-xl border border-neutral-200 bg-blue-600 p-2.5 shadow-xl hover:bg-neutral-100 focus:outline-none dark:border-primary-6000 dark:bg-primary-6000 dark:hover:bg-primary-700 ${
+									className={`z-10 rounded-xl border border-neutral-200 bg-white p-2.5 shadow-xl hover:bg-neutral-100 focus:outline-none dark:border-primary-6000 dark:bg-primary-6000 dark:hover:bg-primary-700 ${
 										open ? 'ring-primary-500 focus:ring-2' : ''
 									}`}
+									onClick={openModalMoreFilter}
 								>
-									 <p>Inquiry Form</p>
+									<CogIcon className="h-8 w-8" />
 								</Popover.Button>
-								
+								<Transition appear show={isOpenMoreFilter} as={Fragment}>
+									<Dialog
+										as="div"
+										className="fixed inset-0 z-50"
+										onClose={closeModalMoreFilter}
+									>
+										<div className="min-h-screen text-center">
+											<TransitionChild
+												as={Fragment}
+												enter="ease-out duration-300"
+												enterFrom="opacity-0"
+												enterTo="opacity-100"
+												leave="ease-in duration-200"
+												leaveFrom="opacity-100"
+												leaveTo="opacity-0"
+											>
+												<div className="fixed inset-0 bg-black bg-opacity-40 dark:bg-opacity-60" />
+											</TransitionChild>
+
+											{/* This element is to trick the browser into centering the modal contents. */}
+											<span
+												className="inline-block h-screen align-middle"
+												aria-hidden="true"
+											>
+												&#8203;
+											</span>
+											<TransitionChild
+												as={'div'}
+												className="inline-block h-screen w-full max-w-4xl px-2 py-8"
+												enter="ease-out duration-300"
+												enterFrom="opacity-0 scale-95"
+												enterTo="opacity-100 scale-100"
+												leave="ease-in duration-200"
+												leaveFrom="opacity-100 scale-100"
+												leaveTo="opacity-0 scale-95"
+											>
+												<div className="h-70 inline-flex w-full max-w-4xl transform flex-col overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all dark:border dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-100">
+													<div className="relative flex-shrink-0 border-b border-neutral-200 px-6 py-4 text-center dark:border-neutral-800">
+														<DialogTitle
+															as="h3"
+															className="text-lg font-medium leading-6 text-gray-900"
+														>
+															Inquiry Now
+														</DialogTitle>
+														<span className="absolute left-3 top-3">
+															<ButtonClose onClick={closeModalMoreFilter} />
+														</span>
+													</div>
+
+													<div className="flex-grow">
+														<form action="#" method="post">
+															<div className="grid grid-cols-1 gap-6 p-6 md:grid-cols-2">
+																<label className="col-span-2 block md:col-span-1">
+																	<Label>Name</Label>
+																	<Input
+																		placeholder="Enter your name"
+																		type="text"
+																		className="mt-1"
+																	/>
+																</label>
+																<label className="col-span-2 block md:col-span-1">
+																	<Label>Surname</Label>
+																	<Input
+																		placeholder="Enter your surname"
+																		type="text"
+																		className="mt-1"
+																	/>
+																</label>
+																<label className="col-span-2 block md:col-span-1">
+																	<Label>Email address</Label>
+																	<Input
+																		type="email"
+																		placeholder="example@example.com"
+																		className="mt-1"
+																	/>
+																</label>
+																<label className="col-span-2 block md:col-span-1">
+																	<Label>Mobile No</Label>
+																	<Input
+																		placeholder="Enter your Mobile Number"
+																		type="number"
+																		className="mt-1"
+																	/>
+																</label>
+																<label className="col-span-2 block">
+																	<Label>Message</Label>
+																	<Textarea
+																		className="mt-1"
+																		rows={6}
+																		placeholder="Enter your message here.."
+																	/>
+																</label>
+															</div>
+														</form>
+													</div>
+
+													<div className="flex flex-shrink-0 items-center justify-between bg-neutral-50 p-6 dark:border-t dark:border-neutral-800 dark:bg-neutral-900">
+														<ButtonThird
+															onClick={closeModalMoreFilter}
+															sizeClass="px-4 py-2 sm:px-5"
+														>
+															Clear
+														</ButtonThird>
+														<ButtonPrimary
+															onClick={closeModalMoreFilter}
+															sizeClass="px-4 py-2 sm:px-5"
+														>
+															Submit
+														</ButtonPrimary>
+													</div>
+												</div>
+											</TransitionChild>
+										</div>
+									</Dialog>
+								</Transition>
 							</>
 						)}
-					</Popover></div>
+					</Popover>
+				</div>
 			</div>
 		)
 	}
