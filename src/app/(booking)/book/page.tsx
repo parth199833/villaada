9c2031b3ced6623ed61337f402f9798f03cb3849
Shubham Pangale <shubham.pangale@agami-tech.com>
2024-08-11 16:12:47 +0530

    Added Booking Form.

--- src/app/(booking)/book/page.tsx
+++ src/app/(booking)/book/page.tsx
@@ -1,99 +1,150 @@
-import React from 'react'
-import Label from '@/components/Label'
-import Avatar from '@/shared/Avatar'
-import ButtonPrimary from '@/shared/ButtonPrimary'
+'use client'
+
+import React, { FC, Fragment, useState } from 'react'
 import Input from '@/shared/Input'
-import Select from '@/shared/Select'
+import Label from '@/components/Label'
 import Textarea from '@/shared/Textarea'
+import ButtonPrimary from '@/shared/ButtonPrimary'
+import Checkbox from '@/shared/Checkbox'
 
-export interface AccountPageProps {}
+export interface CheckOutPagePageMainProps {
+	className?: string
+}
 
-const AccountPage = () => {
-	return (
-		<div className="space-y-6 sm:space-y-8">
-			{/* HEADING */}
-			<h2 className="text-3xl font-semibold">Booking</h2>
-			<div className="w-14 border-b border-neutral-200 dark:border-neutral-700"></div>
-			<div className="flex flex-col md:flex-row">
-				<div className="flex flex-shrink-0 items-start">
-					<div className="relative flex overflow-hidden rounded-full">
-						<Avatar sizeClass="w-32 h-32" />
-						<div className="absolute inset-0 flex cursor-pointer flex-col items-center justify-center bg-black bg-opacity-60 text-neutral-50">
-							<svg
-								width="30"
-								height="30"
-								viewBox="0 0 30 30"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M17.5 5H7.5C6.83696 5 6.20107 5.26339 5.73223 5.73223C5.26339 6.20107 5 6.83696 5 7.5V20M5 20V22.5C5 23.163 5.26339 23.7989 5.73223 24.2678C6.20107 24.7366 6.83696 25 7.5 25H22.5C23.163 25 23.7989 24.7366 24.2678 24.2678C24.7366 23.7989 25 23.163 25 22.5V17.5M5 20L10.7325 14.2675C11.2013 13.7988 11.8371 13.5355 12.5 13.5355C13.1629 13.5355 13.7987 13.7988 14.2675 14.2675L17.5 17.5M25 12.5V17.5M25 17.5L23.0175 15.5175C22.5487 15.0488 21.9129 14.7855 21.25 14.7855C20.5871 14.7855 19.9513 15.0488 19.4825 15.5175L17.5 17.5M17.5 17.5L20 20M22.5 5H27.5M25 2.5V7.5M17.5 10H17.5125"
-									stroke="currentColor"
-									strokeWidth={1.5}
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-
-							<span className="mt-1 text-xs">Change Image</span>
-						</div>
-						<input
-							type="file"
-							className="absolute inset-0 cursor-pointer opacity-0"
-						/>
-					</div>
+const CheckOutPagePageMain: FC<CheckOutPagePageMainProps> = ({
+	className = '',
+}) => {
+	const renderMain = () => {
+		return (
+			<div className="flex w-full flex-col border-neutral-200 shadow-2xl shadow-indigo-500/40 dark:border-neutral-700 sm:rounded-2xl sm:border">
+				<div className="rounded-t-2xl bg-primary-6000 py-6">
+					<h2 className="text-center text-3xl font-semibold text-white lg:text-4xl">
+						Villa Ada
+					</h2>
 				</div>
-				<div className="mt-10 max-w-3xl flex-grow space-y-6 md:mt-0 md:pl-16">
-					<div>
-						<Label>Name</Label>
-						<Input className="mt-1.5" defaultValue="Eden Tuan" />
-					</div>
-					{/* ---- */}
-					<div>
-						<Label>Gender</Label>
-						<Select className="mt-1.5">
-							<option value="Male">Male</option>
-							<option value="Female">Female</option>
-							<option value="Other">Other</option>
-						</Select>
-					</div>
-					{/* ---- */}
-					<div>
-						<Label>Username</Label>
-						<Input className="mt-1.5" defaultValue="@eden_tuan" />
-					</div>
-					{/* ---- */}
-					<div>
-						<Label>Email</Label>
-						<Input className="mt-1.5" defaultValue="example@email.com" />
-					</div>
-					{/* ---- */}
-					<div className="max-w-lg">
-						<Label>Date of birth</Label>
-						<Input className="mt-1.5" type="date" defaultValue="1990-07-22" />
-					</div>
-					{/* ---- */}
-					<div>
-						<Label>Addess</Label>
-						<Input className="mt-1.5" defaultValue="New york, USA" />
-					</div>
-					{/* ---- */}
-					<div>
-						<Label>Phone number</Label>
-						<Input className="mt-1.5" defaultValue="003 888 232" />
-					</div>
-					{/* ---- */}
-					<div>
-						<Label>About you</Label>
-						<Textarea className="mt-1.5" defaultValue="..." />
-					</div>
-					<div className="pt-2">
-						<ButtonPrimary>Update info</ButtonPrimary>
+				<div className="border-b border-neutral-200 dark:border-neutral-700"></div>
+				<div className="space-y-8 px-5 sm:p-6 xl:p-8">
+					<div className="space-y-5">
+						<div className="flex flex-col sm:flex-row sm:space-x-5">
+							<div className="flex-1 space-y-1">
+								<Label>Check In</Label>
+								<Input type="date" defaultValue="MM/YY" />
+							</div>
+							<div className="flex-1 space-y-1">
+								<Label>Check Out</Label>
+								<Input type="date" defaultValue="MM/YY" />
+							</div>
+						</div>
+						<div className="flex flex-col sm:flex-row sm:space-x-5">
+							<div className="flex-1 space-y-1">
+								<Label>Adults </Label>
+								<Input type="number" defaultValue={1} />
+							</div>
+							<div className="flex-1 space-y-1">
+								<Label>Children </Label>
+								<Input type="number" defaultValue={0} />
+							</div>
+						</div>
+						<div className="flex flex-col sm:flex-row sm:space-x-3">
+							<div className="flex-1 space-y-1">
+								<Label>First Name </Label>
+								<Input placeholder="Enter First Name" />
+							</div>
+							<div className="flex-1 space-y-1">
+								<Label>Last Name </Label>
+								<Input placeholder="Enter Last Name" />
+							</div>
+							<div className="flex-1 space-y-1">
+								<Label>Email </Label>
+								<Input type="email" placeholder="Enter Email Adders" />
+							</div>
+							<div className="flex-1 space-y-1">
+								<Label>Phone Number </Label>
+								<Input placeholder="Enter Phone Number" />
+							</div>
+						</div>
+						<div className="space-y-1">
+							<Label>Note to Host</Label>
+							<Textarea placeholder="Enter Here..." />
+							<span className="block text-sm text-neutral-500">
+								Write a few sentences about host.
+							</span>
+						</div>
+						<div className="flex space-x-5">
+							<div className="flex-1 space-y-1">
+								<Label>Discount Code </Label>
+								<div className="flex space-x-3 md:w-1/2">
+									<Input className="" placeholder="Discount Code" />
+									<ButtonPrimary>Apply</ButtonPrimary>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col space-y-4 rounded-lg border p-9 hover:bg-neutral-50 dark:hover:bg-neutral-800">
+							{/* <h3 className="text-2xl font-semibold">Price detail</h3> */}
+							<div className="flex justify-between font-semibold text-neutral-6000 dark:text-neutral-300">
+								<span>Stay Amount</span>
+								<span>$3680</span>
+							</div>
+							<div className="flex justify-between font-semibold text-neutral-6000 dark:text-neutral-300">
+								<span>Taxes</span>
+								<span>$0</span>
+							</div>
+
+							<div className="border-b border-neutral-200 dark:border-neutral-700"></div>
+							<div className="flex justify-between font-semibold">
+								<span>Total</span>
+								<span>$3680</span>
+							</div>
+						</div>
+						<div className="flex space-x-3">
+							<Checkbox name={'1'} />
+							<span>
+								I have read and agree to the{' '}
+								<a
+									href="https://villaadaalmare.com/privacy_policy/"
+									className="text-blue-600"
+									target="_blank"
+								>
+									Privacy Policy
+								</a>
+							</span>
+						</div>
+						<div className="flex space-x-3">
+							<Checkbox name="2" />
+							<span>
+								I have read and agree to the{' '}
+								<a
+									href="https://villaadaalmare.com/term_condition/"
+									className="text-blue-600"
+									target="_blank"
+								>
+									Terms and Conditions
+								</a>
+							</span>
+						</div>
+						<span className="block text-sm italic text-red-600">
+							*You will need to pay in full at the time of booking
+						</span>
+
+						<div className="pt-8 text-center">
+							<ButtonPrimary href={'/pay-done'}>Submit</ButtonPrimary>
+						</div>
 					</div>
 				</div>
 			</div>
+		)
+	}
+
+	return (
+		<div className={`nc-CheckOutPagePageMain ${className}`}>
+			<main className="container mb-24 px-5">
+				<h2 className="mb-5 mt-5 text-center text-3xl font-semibold lg:text-4xl">
+					Booking Form
+				</h2>
+				<div className="w-full lg:pr-10">{renderMain()}</div>
+			</main>
 		</div>
 	)
 }
 
-export default AccountPage
+export default CheckOutPagePageMain
